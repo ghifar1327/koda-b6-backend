5e023980e8ed14dde3715d6ff12ae1400f7f1b38
ghifar1327 <givartorreto@gmail.com>
2026-03-07 01:57:50 +0700

    feat: add update user profile service

--- internal/service/user.go
+++ internal/service/user.go
@@ -57,5 +57,42 @@ func (s *UserService) Register(ctx context.Context, req dto.RegisterRequest) err
 		CreatedAt: time.Now(),
 	}
 
-	return s.repo.Create(ctx, newUser)
+	return s.repo.CreateUser(ctx, newUser)
+}
+
+func (s *UserService) UpdateUserProfile(ctx context.Context, id uuid.UUID, req dto.UpdateUsersRequest) error {
+	user, err := s.repo.GetUserByID(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	if strings.TrimSpace(req.Picture) != "" {
+		user.Picture.String = req.Picture
+	}
+
+	if strings.TrimSpace(req.FullName) != "" {
+		user.FullName = req.FullName
+	}
+
+	if strings.TrimSpace(req.Email) != "" {
+		if !strings.Contains(req.Email, "@") || !strings.Contains(req.Email, ".") {
+			return errors.New("invalid email format")
+		}
+		user.Email = req.Email
+	}
+	if strings.TrimSpace(req.Password) != "" && len(req.Password) > 5 {
+		newPwd , err :=  utils.HashPassword(req.Password)
+		if err != nil {
+			return errors.New("password must be at least 5 characters")
+		}
+		user.Password = newPwd
+	}
+	if strings.TrimSpace(req.Address) != "" {
+		user.Address = req.Address
+	}
+	if strings.TrimSpace(req.Phone) != "" {
+		user.Phone = req.Phone
+	}
+
+	return s.repo.UpdateUser(ctx, id, *user)
 }
