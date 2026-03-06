c9340a62e5e5fcf84a14147363bff8456f08100c
ghifar1327 <givartorreto@gmail.com>
2026-03-07 01:55:31 +0700

    feat: implement get and update user queries

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -3,8 +3,10 @@ package repository
 import (
 	"backend/internal/models"
 	"context"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
 )
 
@@ -18,7 +20,7 @@ func NewUserrepository(db *pgx.Conn) *UserRepository {
 	}
 }
 
-func (r *UserRepository) GetAll(ctx *gin.Context) ([]models.User, error) {
+func (r *UserRepository) GetAllUser(ctx *gin.Context) ([]models.User, error) {
 	query := `SELECT id, full_name, picture, email,password ,role_id ,phone, address, created_at, updated_at FROM users`
 
 	rows, err := r.db.Query(ctx, query)
@@ -40,10 +42,51 @@ func (r *UserRepository) GetAll(ctx *gin.Context) ([]models.User, error) {
 	return users, nil
 }
 
-// Create User
-func (r *UserRepository) Create(ctx context.Context, u models.User) error {
+// ==================================================================================================================================================== Get User By ID
+func (r *UserRepository) GetUserByID(ctx context.Context, id uuid.UUID) (*models.User, error) {
+	query := `SELECT id, picture, full_name, email, password,address, phone, role_id FROM users WHERE id=$1`
+	var user models.User
+
+	err := r.db.QueryRow(ctx, query, id).Scan(&user.Id, &user.Picture, &user.FullName, &user.Email, &user.Password, user.Address, &user.RoleId, &user.Phone)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+// ====================================================================================================================================================  Create User
+
+func (r *UserRepository) CreateUser(ctx context.Context, u models.User) error {
 	query := `INSERT INTO users (id, full_name, email, password, address, phone, role_id,created_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`
-	_, err := r.db.Exec(ctx, query, u.Id, u.FullName, u.Email, u.Password, u.Address, u.Phone, u.RoleId ,u.CreatedAt)
+	_, err := r.db.Exec(ctx, query, 
+		u.Id, 
+		u.FullName, 
+		u.Email, 
+		u.Password, 
+		u.Address, 
+		u.Phone, 
+		u.RoleId, 
+		u.CreatedAt)
+
+	return err
+}
+
+// ==================================================================================================================================================== Update User
+
+func (r *UserRepository) UpdateUser(ctx context.Context, id uuid.UUID, u models.User) error {
+	query := `UPDATE users 
+SET picture=$1, full_name=$2, email=$3, password=$4, address=$5, phone=$6, role_id=$7, updated_at=$8 
+WHERE id=$9`
+	_, err := r.db.Exec(ctx, query,
+		u.Picture,
+		u.FullName,
+		u.Email,
+		u.Password,
+		u.Address,
+		u.Phone,
+		u.RoleId,
+		time.Now(),
+		id)
 	return err
 }
 
