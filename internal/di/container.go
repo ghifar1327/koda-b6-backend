ad036c6cde941121cedc4f414d297a709ca7e6d8
ghifar1327 <givartorreto@gmail.com>
2026-03-10 02:14:42 +0700

    feat(product): add product dependency injection in container

--- internal/di/container.go
+++ internal/di/container.go
@@ -15,6 +15,11 @@ type Container struct {
 	userRepo    *repository.UserRepository
 	userService *service.UserService
 	userHandler *handlers.UserHandler
+
+	// PRODUCT
+	productRepo    *repository.ProductRepository
+	productService *service.ProductService
+	productHandler *handlers.ProductHandler
 }
 
 func NewContainer(db *pgx.Conn) *Container {
@@ -33,8 +38,17 @@ func (c *Container) initDependencies() {
 	c.userRepo = repository.NewUserrepository(c.db)
 	c.userService = service.NewUserService(c.userRepo)
 	c.userHandler = handlers.NewUserhadler(c.userService)
+
+	//PRODUCT
+	c.productRepo = repository.NewProductRepository(c.db)
+	c.productService = service.NewProductService(c.productRepo)
+	c.productHandler = handlers.NewProductHandler(c.productService)
 }
 
 func (c *Container) UserHandler() *handlers.UserHandler {
 	return c.userHandler
+}
+
+func (c *Container) ProductHandler() *handlers.ProductHandler{
+	return c.productHandler
 }
