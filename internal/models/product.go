6b8447193ec1574078ecbbe128711776a8ddfec0
ghifar1327 <givartorreto@gmail.com>
2026-03-12 04:34:24 +0700

    feat: add struct review product, review recommended product

--- internal/models/product.go
+++ internal/models/product.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Product struct {
 	Id          int       `json:"id"`
@@ -13,110 +17,30 @@ type Product struct {
 	UploadedAt  time.Time `json:"updated_at"`
 }
 
-// package models
+type ReviewProduct struct {
+	Id                  int       `db:"id"`
+	UserId              uuid.UUID `db:"user_id"`
+	IdTransactionDetail int       `db:"id_transaksion_detail"`
+	Rating              float64   `db:"rating"`
+	Message             string    `db:"message"`
+}
 
-// import "time"
 
-// type Method struct {
-// 	Id       int
-// 	Name     string
-// 	AddPrice int
-// }
+type Reviews struct {
+	Id          int     `db:"id"`
+	Name        string  `db:"name"`
+	Image       string  `db:"url"`
+	Description string  `db:"description"`
+	Price       int     `db:"price"`
+	Rating      float64 `db:"rating"`
+}
 
-// type Size struct {
-// 	Id       int
-// 	Name     string
-// 	AddPrice int
-// }
-// type Variant struct {
-// 	Id       int
-// 	Name     string
-// 	AddPrice int
-// }
-
-// type Product struct {
-// 	Id        int    `json:"id"`
-// 	Name      string `json:"name"`
-// 	BasePrice int    `json:"base_price"`
-// 	Variants  []int  `json:"variants"`
-// 	Sizes     []int  `json:"sizes"`
-// 	Methods   []int  `json:"methods"`
-// 	Stock     int    `json:"stock"`
-// }
-
-// type CartItem struct {
-// 	Id        int `json:"id"`
-// 	UserID    int `json:"user_id"`
-// 	ProductID int `json:"product_id"`
-// 	VariantID int `json:"variant_id"`
-// 	SizeID    int `json:"size_id"`
-// 	MethodID  int `json:"method_id"`
-// 	Qty       int `json:"qty"`
-// 	Price     int `json:"price"`
-// }
-
-// type OrderItem struct {
-// 	Id        int
-// 	ProductID int
-// 	Qty       int
-// 	Price     int
-// }
-
-// type Order struct {
-// 	Id            int         `json:"id"`
-// 	UserID        int         `json:"user_id"`
-// 	Total         int         `json:"total"`
-// 	Status        string      `json:"status"`
-// 	Address       string      `json:"address"`
-// 	PaymentMethod string      `json:"payment_method"`
-// 	CreatedAt     time.Time   `json:"created_at"`
-// 	Items         []OrderItem `json:"items"`
-// }
-
-// // ===================================================================== requests
-
-// // ===================================================================== methode
-
-// func (s Size) Render(ids []int) []string {
-// 	var result []string
-
-// 	for _, id := range ids {
-// 		for _, size := range Sizes {
-// 			if size.Id == id {
-// 				result = append(result, size.Name)
-// 				break
-// 			}
-// 		}
-// 	}
-
-// 	return result
-// }
-
-// func (v Variant) Render(idv []int) []string {
-// 	var result []string
-// 	for _, id := range idv {
-// 		for _, variant := range Variants {
-// 			if variant.Id == id {
-// 				result = append(result, variant.Name)
-// 				break
-// 			}
-// 		}
-// 	}
-// 	return result
-// }
-
-// func (m Method) Render(idm []int) []string {
-// 	var result []string
-// 	for _, id := range idm {
-// 		for _, method := range Methods {
-// 			if method.Id == id {
-// 				result = append(result, method.Name)
-// 			}
-// 		}
-// 	}
-// 	return result
-// }
-
-// type Renders interface {
-// 	Render() []string
-// }
+type RecommendedProduct struct {
+	Id          int     `db:"id"`
+	Name        string  `db:"name"`
+	Image       string  `db:"url"`
+	Description string  `db:"description"`
+	Price       int     `db:"price"`
+	TotalReview int     `db:"total_review"`
+	AvgRating   float64 `db:"avg_rating"`
+}
