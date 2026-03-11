6c6b8ed233ca7c91f5467ec7907e10f769bc9378
ghifar1327 <givartorreto@gmail.com>
2026-03-11 09:19:38 +0700

    feat: add update product add delete product, service

--- internal/service/product.go
+++ internal/service/product.go
@@ -1,10 +1,11 @@
 package service
 
 import (
-	"backend/internal/dto"
 	"backend/internal/models"
 	"backend/internal/repository"
 	"context"
+	"errors"
+	"strings"
 	"time"
 )
 
@@ -26,8 +27,8 @@ func (s *ProductService) GetAllProductByID(ctx context.Context, id int) (*models
 	return s.repo.GetProductByID(ctx, id)
 }
 
-func (s *ProductService) CreateProduct(ctx context.Context, req dto.CreteProductRequest) error {
-	newProduct := dto.CreteProductRequest{
+func (s *ProductService) CreateProduct(ctx context.Context, req models.Product) error {
+	newProduct := models.Product{
 		Name:        req.Name,
 		Description: req.Description,
 		Price:       req.Price,
@@ -35,4 +36,35 @@ func (s *ProductService) CreateProduct(ctx context.Context, req dto.CreteProduct
 		CreatedAt:   time.Now(),
 	}
 	return s.repo.CreateProduct(ctx, newProduct)
+}
+
+func (s *ProductService) UpdateProduct(ctx context.Context, id int, req models.Product) error {
+	product, err := s.repo.GetProductByID(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	if strings.TrimSpace(req.Name) != "" {
+		product.Name = req.Name
+	}
+
+	if strings.TrimSpace(req.Description) != "" {
+		product.Description = req.Description
+	}
+
+	if req.Stoct <= 0 {
+		return errors.New("invalid Price")
+	}
+	product.Price = req.Price
+
+	if req.Stoct <= 0 {
+		return errors.New("invalid stock")
+	}
+	product.Stoct = req.Stoct
+
+	return s.repo.UpdateProduct(ctx, id, *product)
+}
+
+func (s *ProductService) DeleteProduct(ctx context.Context, id int) error {
+	return s.repo.DeleteProduct(ctx, id)
 }
