d268585ceb7ab265bfffd3b87dd775251ecd4634
ghifar1327 <givartorreto@gmail.com>
2026-03-11 10:20:20 +0700

    feat(forgot-password): add create, get by email, and delete reset code repository

---
+++ internal/repository/forgotPwd.go
@@ -0,0 +1,52 @@
+package repository
+
+import (
+	"backend/internal/models"
+	"context"
+
+	"github.com/jackc/pgx/v5"
+)
+
+type DataRepository struct {
+	db *pgx.Conn
+}
+
+func NewDataRepository(db *pgx.Conn) *DataRepository {
+	return &DataRepository{
+		db: db,
+	}
+}
+
+func (r *UserRepository) CreateData(ctx context.Context, f models.ForgotPassword) error {
+	query := `INSERT INTO forgot_pwd (email, code) VALUES ($1, $2)`
+	_, err := r.db.Exec(ctx, query, f.Email, f.Code)
+
+	return err
+}
+
+func (r *UserRepository) GetDataByEmail(ctx context.Context, email string) (*models.ForgotPassword, error) {
+	query := `
+		SELECT 
+		email,
+		code
+			  FROM forgot_pwd email=$1`
+
+	var fp models.ForgotPassword
+
+	err := r.db.QueryRow(ctx, query, email).Scan(
+		fp.Email,
+		fp.Code,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &fp, nil
+}
+
+func (r *UserRepository) DeleteDataByCode(ctx context.Context, code int) error {
+	query := `DELETE FROM forgot_pwd WHERE code=$1`
+	_, err := r.db.Exec(ctx, query, code)
+	return err
+}
