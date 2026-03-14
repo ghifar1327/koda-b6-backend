ffa3c9803790a91c2b9786d38644c89ef3edac2c
ghifar1327 <givartorreto@gmail.com>
2026-03-14 14:22:25 +0700

    feat(models): add transaction models with item details and transaction row mapping

--- internal/models/transaction.go
+++ internal/models/transaction.go
@@ -7,11 +7,42 @@ import (
 )
 
 type Transaction struct {
-	Id             uuid.UUID `json:"id"`
-	UserId         uuid.UUID `json:"user_id"`
-	Status         string    `json:"status"`
-	IdMethod       string    `json:"id_method"`
-	PaymentMethode string    `json:"payment_method"`
-	IdVoucher      int       `json:"id_voucher"`
-	CreatedAt      time.Time `json:"created_at"`
+	Id               uuid.UUID    `json:"id"`
+	FullName         string       `json:"full_name"`
+	Address          string       `json:"address"`
+	Phone            string       `json:"phone"`
+	Shipping         string       `json:"shipping"`
+	PaymentMethod    string       `json:"payment_method"`
+	Status           string       `json:"status"`
+	TotalTransaction int          `json:"total_transaction"`
+	Items            []ItemDetail `json:"items"`
+	CreatedAt        time.Time    `json:"created_at"`
+}
+
+type ItemDetail struct {
+	TransactionId uuid.UUID `json:"transaction_id"`
+	ProductImage  string    `json:"product_image"`
+	ProductName   string    `json:"product_name"`
+	Size          string    `json:"size"`
+	Variant       string    `json:"variant"`
+	Quantity      int       `json:"quantity"`
+	SubTotal      int       `json:"subtotal"`
+}
+
+type TransactionRow struct {
+	Id            uuid.UUID `db:"id"`
+	FullName      string    `db:"full_name"`
+	Address       string    `db:"address"`
+	Phone         string    `db:"phone"`
+	Shipping      string    `db:"shipping"`
+	PaymentMethod string    `db:"payment_method"`
+	Status        string    `db:"status"`
+	CreatedAt     time.Time `db:"created_at"`
+
+	ProductName  string `db:"product_name"`
+	ProductImage string `db:"product_image"`
+	Size         string `db:"size"`
+	Variant      string `db:"variant"`
+	Quantity     int    `db:"quantity"`
+	Subtotal     int    `db:"subtotal"`
 }
