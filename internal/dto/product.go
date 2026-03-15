ba5c98b88c8d56dd2f04f050ff5b722c7fb6693a
ghifar1327 <givartorreto@gmail.com>
2026-03-15 17:11:19 +0700

    feat add create product struck for request api

---
+++ internal/dto/product.go
@@ -0,0 +1,10 @@
+package dto
+
+type CreateProductRequest struct {
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Price       int      `json:"price"`
+	Categories  []string `json:"categories"`
+	Stoct       int      `json:"stock"`
+}
+
