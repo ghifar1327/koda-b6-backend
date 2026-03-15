3acc97080d116c7bd49b85dd0f23fde67b23d357
ghifar1327 <givartorreto@gmail.com>
2026-03-15 17:24:37 +0700

    feat: integrate swagger documentation in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -11,6 +11,12 @@ import (
 	"github.com/joho/godotenv"
 )
 
+
+// @title Coffee Shop API
+// @version 1.0
+// @description This is a coffee shop backend API
+// @host localhost:8888
+// @BasePath /
 func main() {
 	godotenv.Load()
 	dbURL := os.Getenv("DATABASE_URL")
@@ -28,38 +34,3 @@ func main() {
 
 	r.Run(":8888")
 }
-
-// import (
-// 	"backend/internal/handlers"
-// 	"backend/internal/middleware"
-
-// 	"github.com/gin-gonic/gin"
-// 	"github.com/joho/godotenv"
-// )
-
-// func main() {
-// 	godotenv.Load()
-// 	// ===================================================================== CRUD USER
-// 	r := gin.Default()
-// 	r.Use(middleware.CorsMiddleware())
-// 	users := r.Group("/users")
-// 	users.POST("", handlers.CreateUser)
-// 	users.GET("", handlers.GetAllUser)
-// 	users.GET("/:id", handlers.GetUserByID)
-// 	users.PATCH("/:id", handlers.UpdateUser)
-// 	users.DELETE("/:id", handlers.DeleteUser)
-
-// 	// ================================================================ CRUD PRODUCT
-// 	products := r.Group("/products")
-// 	products.GET("/products", handlers.GetAllProduct)
-
-// 	// ===================================================================== Feature auth
-// 	r.POST("/login", handlers.AuthLogin)
-
-// 	// ====================================================================== FEATURE ORDER
-// 	orders := r.Group("/orders")
-// 	orders.POST("/addcart", handlers.AddChart)
-// 	orders.POST("/checkout", handlers.Checkout)
-
-// 	r.Run("localhost:8888")
-// }
