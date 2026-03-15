be8c985c738417e71874e527452ff2d669f981dd
ghifar1327 <givartorreto@gmail.com>
2026-03-15 17:10:03 +0700

    feat: add user response struck

--- internal/dto/response.go
+++ internal/dto/response.go
@@ -1,6 +1,14 @@
 package dto
 
+import "github.com/google/uuid"
+
 type Response struct {
-	Ok      bool
-	Message string
+	Success bool   `json:"success"`
+	Message string `jason:"message"`
+}
+
+type UserResponse struct {
+	Id       uuid.UUID `json:"id"`
+	FullName string    `json:"name"`
+	Email    string    `json:"email"`
 }
