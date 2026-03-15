1b9773f344904b8182f23c8ee32fc78487d9962e
ghifar1327 <givartorreto@gmail.com>
2026-03-16 02:03:53 +0700

    feat: add JWT auth middleware, fixed header cors middleware

---
+++ internal/middleware/auth.middleware.go
@@ -0,0 +1,51 @@
+package middleware
+
+import (
+	"backend/internal/dto"
+	"backend/internal/utils"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		authHeader := ctx.GetHeader("Authorization")
+
+		if authHeader == "" {
+			ctx.JSON(http.StatusUnauthorized, dto.Response{
+				Success: false,
+				Message: "Authorization header required",
+			})
+			ctx.Abort()
+			return
+		}
+
+		if !strings.HasPrefix(authHeader, "Bearer ") {
+			ctx.JSON(http.StatusUnauthorized, dto.Response{
+				Success: false,
+				Message: "Invalid token format",
+			})
+			ctx.Abort()
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		claims, err := utils.VerifyToken(tokenString)
+		if err != nil {
+			ctx.JSON(http.StatusUnauthorized, dto.Response{
+				Success: false,
+				Message: "Invalid or expired token",
+			})
+			ctx.Abort()
+			return
+		}
+
+		ctx.Set("userId", claims.Id)
+
+		ctx.Next()
+	}
+}
--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -8,12 +8,16 @@ import (
 
 func CorsMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+
 		ctx.Header("Access-Control-Allow-Origin", "http://localhost:5173")
-		ctx.Header("Access-Control-Allow-Headers", "content-type")
+		ctx.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		ctx.Header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
+
 		if ctx.Request.Method == "OPTIONS" {
-			ctx.Data(http.StatusOK, "", []byte(""))
-		} else {
-			ctx.Next()
+			ctx.AbortWithStatus(http.StatusOK)
+			return
 		}
+
+		ctx.Next()
 	}
 }
