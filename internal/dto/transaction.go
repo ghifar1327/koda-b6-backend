ce8c46c514e49c7d05a62c3c6b5178591f3d36ff
ghifar1327 <givartorreto@gmail.com>
2026-03-16 14:57:23 +0700

    create struck create item, create transaction

--- internal/dto/transaction.go
+++ internal/dto/transaction.go
@@ -5,19 +5,19 @@ import "github.com/google/uuid"
 type UpdateTransactionRequst struct {
 	Status string `json:"status"`
 }
-type CreateRransactionRequest struct {
-	Id            uuid.UUID `json:"id"`
-	UserId        uuid.UUID `json:"user_id"`
-	Status        string    `json:"status"`
-	PaymentMethod string    `json:"payment_method"`
-	IdMethod      int       `json:"id_method"`
-	IdVoucher     *int      `json:"id_voucher"`
+
+type CreateTransactionRequest struct {
+	UserId        uuid.UUID           `json:"user_id"`
+	IdMethod      int                 `json:"id_method"`
+	PaymentMethod string              `json:"payment_method"`
+	IdVoucher     *int                `json:"id_voucher"`
+	Items         []CreateItemRequest `json:"items"`
 }
 
-type AddItemRequiest struct {
-	TransactionId uuid.UUID `json:"transaction_id"`
-	ProductId     int       `json:"product_id"`
-	SizeId        int       `json:"size_id"`
-	VariantId     int       `json:"variant_id"`
-	Quantity      int       `json:"quantity"`
+type CreateItemRequest struct {
+	
+	ProductId int `json:"product_id"`
+	SizeId    int `json:"size_id"`
+	VariantId int `json:"variant_id"`
+	Quantity  int `json:"quantity"`
 }
