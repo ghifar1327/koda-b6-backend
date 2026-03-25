30ec5f90f85cf90b9d4049580724204fc1d38ed9
ghifar1327 <givartorreto@gmail.com>
2026-03-26 00:57:57 +0700

    feat(transactions): add update and delete transaction handlers with swager docs

--- internal/handlers/transaction.go
+++ internal/handlers/transaction.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"backend/internal/dto"
+	"backend/internal/models"
 	"backend/internal/service"
 	"net/http"
 
@@ -19,7 +20,6 @@ func NewTransactionHandler(s *service.TransactionService) *TransactionHandler {
 	}
 }
 
-
 // GetAllTransactions godoc
 // @Summary Get all transactions
 // @Description Retrieve all transactions
@@ -32,12 +32,18 @@ func NewTransactionHandler(s *service.TransactionService) *TransactionHandler {
 func (h *TransactionHandler) GetAllTransactions(ctx *gin.Context) {
 	Transactions, err := h.service.GetAllTransactions(ctx.Request.Context())
 	if err != nil {
-		ctx.JSON(500, gin.H{"error": err.Error()})
+		ctx.JSON(500, dto.Response{
+			Success: false,
+			Message: err.Error(),
+		})
 		return
 	}
-	ctx.JSON(200, Transactions)
+	ctx.JSON(200, dto.ResponseWrap{
+		Success: true,
+		Message: "List of Transaction",
+		Results: Transactions,
+	})
 }
-
 
 // GetTransactionByID godoc
 // @Summary Get transaction by ID
@@ -53,18 +59,25 @@ func (h *TransactionHandler) GetAllTransactions(ctx *gin.Context) {
 func (h *TransactionHandler) GetTransactionbyID(ctx *gin.Context) {
 	id, err := uuid.Parse(ctx.Param("id"))
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		ctx.JSON(http.StatusBadRequest, dto.Response{
+			Success: false,
+			Message: "invalid ID",
+		})
 	}
 	Transaction, err := h.service.GetAllTransactionByID(ctx.Request.Context(), id)
 	if err != nil {
-		ctx.JSON(http.StatusNotFound, gin.H{
-			"error": "user not found",
+		ctx.JSON(http.StatusNotFound, dto.Response{
+			Success: false,
+			Message: "transaction not found",
 		})
 		return
 	}
-	ctx.JSON(http.StatusOK, Transaction)
+	ctx.JSON(http.StatusOK, dto.ResponseWrap{
+		Success: true,
+		Message: "Data of transaction",
+		Results: Transaction,
+	})
 }
-
 
 // CreateTransaction godoc
 // @Summary Create transaction
@@ -97,5 +110,87 @@ func (h *TransactionHandler) CreateTransaction(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, dto.Response{
 		Success: true,
 		Message: "Create Transaction successfully",
+	})
+}
+
+
+// UpdateTransaction godoc
+// @Summary Update transaction
+// @Description Update transaction by ID
+// @Tags Transactions
+// @Accept json
+// @Produce json
+// @Param id path string true "Transaction ID (UUID)"
+// @Param request body models.Transaction true "Updated Transaction Data"
+// @Success 200 {object} dto.Response
+// @Failure 400 {object} dto.Response
+// @Router admin/transactions/{id} [patch]
+func (h *TransactionHandler) UpdateTransaction(ctx *gin.Context) {
+	id, err := uuid.Parse(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, dto.Response{
+			Success: false,
+			Message: "invalid ID",
+		})
+		return
+	}
+	
+	var req models.Transaction
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, dto.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	if err := h.service.UpdateTransaction(ctx.Request.Context(), id , req); err != nil{
+		ctx.JSON(http.StatusBadRequest, dto.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, dto.Response{
+		Success: true,
+		Message: "Transactions updated successfully",
+	})
+}
+
+
+// DeleteTransaction godoc
+// @Summary Delete transaction
+// @Description Delete transaction by ID
+// @Tags Transactions
+// @Accept json
+// @Produce json
+// @Param id path string true "Transaction ID (UUID)"
+// @Success 200 {object} dto.Response
+// @Failure 400 {object} dto.Response
+// @Router admin/transactions/{id} [delete]
+func (h *TransactionHandler) DeleteTransaction(ctx *gin.Context) {
+	idParam := ctx.Param("id")
+
+	id, err := uuid.Parse(idParam)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, dto.Response{
+			Success: false,
+			Message: "Invalid Transaction Id",
+		})
+		return
+	}
+
+	if err := h.service.DeleteTransaction(ctx.Request.Context(), id); err != nil {
+		ctx.JSON(http.StatusBadRequest, dto.Response{
+			Success: false,
+			Message: "failed to delete Transaction",
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, dto.Response{
+		Success: true,
+		Message: "Transaction delete successfully",
 	})
 }
