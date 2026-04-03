852fb8b624360fe10e1304b83a53320fce1710e3
ghifar1327 <givartorreto@gmail.com>
2026-04-04 02:44:54 +0700

    fix: type field struct

--- internal/models/cart.go
+++ internal/models/cart.go
@@ -1,10 +1,11 @@
 package models
 
 type Cart struct {
-	Image       string `json:"product_image" db:"product_image"`
-	ProductName string `json:"product_name" db:"product_name"`
-	Size        string `json:"size" db:"size"`
-	Variant     string `json:"variant" db:"variant"`
-	Quantity    string `json:"quantity" db:"quantity"`
-	Subtotal    string `json:"subtotal" db:"subtotal"`
+	Id          int     `json:"id" db:"id"`
+	Image       string  `json:"product_image" db:"product_image"`
+	ProductName string  `json:"product_name" db:"product_name"`
+	Size        string  `json:"size" db:"size"`
+	Variant     string  `json:"variant" db:"variant"`
+	Quantity    int     `json:"quantity" db:"quantity"`
+	Subtotal    float64 `json:"subtotal" db:"subtotal"`
 }
