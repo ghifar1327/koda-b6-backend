9f9bcf10c127299972045b3454044515b35c1b06
ghifar1327 <givartorreto@gmail.com>
2026-04-05 04:17:58 +0700

    feat: add  update picture, add  update password

--- internal/repository/auth.go
+++ internal/repository/auth.go
@@ -3,7 +3,10 @@ package repository
 import (
 	"backend/internal/models"
 	"context"
+	"fmt"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
 )
@@ -48,8 +51,44 @@ func (r *AuthRepository) GetForgotPWDByEmail(ctx context.Context, email string) 
 	return &fp, nil
 }
 
+func (r *AuthRepository) UpdatePassword(ctx context.Context, id uuid.UUID, newPassword string) error {
+	query := `UPADATE users SET password = $1 WHERE is = 2`
+	_, err := r.db.Exec(ctx, query, newPassword, id)
+	return err
+}
+
 func (r *AuthRepository) DeleteForgotPWDByCode(ctx context.Context, code int) error {
 	query := `DELETE FROM forgot_password WHERE code=$1`
 	_, err := r.db.Exec(ctx, query, code)
 	return err
+}
+
+
+func (r *UserRepository) UpdateUserPicture(ctx context.Context, id uuid.UUID, picture string) (models.User, error) {
+	query := `
+	    UPDATE users 
+	    SET 
+	    	picture = $1,
+	    	updated_at = $2
+	    WHERE id = $3
+	`
+
+	_, err := r.db.Exec(ctx, query,
+		picture,
+		time.Now(),
+		id,
+	)
+	if err != nil {
+		return models.User{}, err
+	}
+
+	// clear cache
+	r.rdb.Del(ctx, fmt.Sprintf("user:id:%s", id.String()))
+
+	user, err := r.GetUserByID(ctx, id)
+	if err != nil {
+		return models.User{}, err
+	}
+
+	return *user, nil
 }
