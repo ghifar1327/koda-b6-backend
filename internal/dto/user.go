62df0acb6071068d1d1a2706871f14d8cf1ddf55
ghifar1327 <givartorreto@gmail.com>
2026-04-05 22:52:42 +0700

    fix: delete user response

--- internal/dto/user.go
+++ internal/dto/user.go
@@ -1,9 +1,5 @@
 package dto
 
-import (
-	"github.com/google/uuid"
-)
-
 type UpdateUsersRequest struct {
 	Picture  string `json:"picture"`
 	Email    string `json:"email"`
@@ -12,14 +8,4 @@ type UpdateUsersRequest struct {
 	Address  string `json:"address"`
 	Phone    string `json:"phone"`
 	RoleId   int    `json:"role_id"`
-}
-
-type userResponse struct {
-	Id       uuid.UUID `json:"id"`
-	Picture  string    `json:"picture"`
-	FullName string    `json:"full_name"`
-	Email    string    `json:"email"`
-	Address  string    `json:"address"`
-	RoleId   int       `json:"role_id"`
-	Phone    string    `json:"phone"`
 }
