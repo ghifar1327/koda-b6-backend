7c5c98f9cea475a33dd40ec88aba05d5d9782044
ghifar1327 <givartorreto@gmail.com>
2026-04-14 13:47:51 +0700

    feat: support multiple origins via env CORS_ORIGINS

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"encoding/json"
 	"net/http"
 	"os"
 
@@ -8,14 +9,23 @@ import (
 )
 
 func CorsMiddleware() gin.HandlerFunc {
-	// godotenv.Load()
-	return func(ctx *gin.Context) {
+	var origins []string
+	_ = json.Unmarshal([]byte(os.Getenv("CORS_ORIGINS")), &origins)
 
-		ctx.Header("Access-Control-Allow-Origin", os.Getenv("FRONTEND_URL"))
+	return func(ctx *gin.Context) {
+		origin := ctx.GetHeader("Origin")
+
+		for _, o := range origins {
+			if o == origin {
+				ctx.Header("Access-Control-Allow-Origin", origin)
+				break
+			}
+		}
+
 		ctx.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		ctx.Header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
 
-		if ctx.Request.Method == "OPTIONS" {
+		if ctx.Request.Method == http.MethodOptions {
 			ctx.AbortWithStatus(http.StatusOK)
 			return
 		}
